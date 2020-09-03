4126cc45cacb416967a5f0bdc465616ed29deb0d
Israel Castro <israel.castro@gmail.com>
2020-09-03 14:14:57 -0500

    [Add] object JS styles

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,21 +1,40 @@
 import React, { Component } from 'react';
 
 class Task extends Component {
+
+	StyleCompleted() {
+		return {
+			fontSize: '20px',
+			color: this.props.task.done ? 'gray' : 'black',
+			textDecoration: this.props.task.done ? 'line-through' : 'none'
+		}
+	}
+
 	render() {
 
 		const {task} = this.props;
 
 		return (
-			<div>
+			<div style={this.StyleCompleted()}>
 				{task.title} -
 				{task.description} -
 				{task.done} -
 				{task.id}
 				<input type="checkbox" />
-				<button>x</button>
+				<button style={btnDelete}>x</button>
 			</div>
 		)
 	}
+}
+
+const btnDelete = {
+	fontSize: '18px',
+	background: '#ea2027',
+	color: '#fff',
+	border: 'none',
+	padding: '10px 15px',
+	borderRadius: '50%',
+	cursor: 'pointer'
 }
 
 export default Task;
