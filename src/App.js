329e66d520419fbb7730d643cfcd7b46a8380f21
Israel Castro <israel.castro@gmail.com>
2020-09-03 13:48:56 -0500

    [Add] task feature

--- src/App.js
+++ src/App.js
@@ -1,54 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-// function Helloworld(props) {
-//   return (
-//     <div className="hello">
-//       <h3>{props.text}</h3>
-//       <p>{props.subtitle}</p>
-//     </div>
-//   )
-// }
+import tasks from './sample/task.json';
 
-class Helloworld extends React.Component {
+import Tasks from './components/Tasks'
+
+class App extends Component {
 
   state = {
-    show: true
-  }
-
-  toggleShow = () => {
-    this.setState({show: !this.state.show})
+    tasks: tasks
   }
 
   render() {
-    if (this.state.show) {
-      return (
-        <div className="hello">
-          <h3>{this.props.text}</h3>
-          <p>{this.props.subtitle}</p>
-          <button onClick={this.toggleShow}>Toggle show</button>
-        </div>
-      )
-    } else {
-      return (
-        <div className="hello">
-          <h1>There are not elements</h1>
-          <button onClick={this.toggleShow}>Toggle show</button>
-        </div>
-      )
-    }
+    return (
+      <div>
+        <Tasks tasks={this.state.tasks} />
+      </div>
+    )
   }
-}
-
-function App() {
-  return (
-    <div>
-      This is my component:
-      <Helloworld text="Hello Fazt" subtitle="Lorem ipsum" />
-      <Helloworld text="Second component" subtitle="This is another subtitle" />
-      <Helloworld text="Third component" subtitle="Just another subtitle" />
-    </div>
-  );
 }
 
 export default App;
